237a5950d4e99773714b8f3c6bf10b8110897ec5
Terence Lee <hone02@gmail.com>
2013-01-23 15:02:52 -0800

    don't need to do anything for an anvil app in setup

--- lib/hatchet/anvil_app.rb
+++ lib/hatchet/anvil_app.rb
@@ -1,10 +1,5 @@
 module Hatchet
   class AnvilApp < App
-    def setup!
-      super
-      heroku.post_app(name: name)
-    end
-
     def push!
       Anvil::Engine.build(".", :buildpack => @buildpack)
     end
