fcb3fc3bdc6ce8adb5445a1c43f2aa2fcaf5eed1
schneems <richard.schneeman@gmail.com>
2013-07-24 17:02:27 -0500

    update rake hatchet:setup_travis

--- lib/hatchet/tasks.rb
+++ lib/hatchet/tasks.rb
@@ -1,16 +1,23 @@
 require 'rake'
 require 'rake/testtask'
 
+config_ssh = <<-RUBY
+Host heroku.com
+    StrictHostKeyChecking no
+    CheckHostIP no
+    UserKnownHostsFile=/dev/null
+Host github.com
+    StrictHostKeyChecking no
+RUBY
+
 namespace :hatchet do
   task :setup_travis do
     puts "== Setting Up Travis =="
     ['bundle exec hatchet install',
-     %Q{echo "\nHost heroku.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config},
-     %Q{echo "\nHost github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config},
+     "echo '#{config_ssh}' >> ~/.ssh/config",
      'wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh',
-     'ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa',
-     'heroku keys:add',
-     'heroku login'
+     'heroku keys:clear',
+     'yes | heroku keys:add'
     ].each do |command|
       puts "== Running: #{command}"
       `#{command}`
