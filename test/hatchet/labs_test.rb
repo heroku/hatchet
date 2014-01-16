20d50fe4ef19333c134bed46b4ed0f5b89c7417d
schneems <richard.schneeman@gmail.com>
2014-01-16 13:45:38 -0600

    Add ability to set and get labs

---
+++ test/hatchet/labs_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class LabsTest < Test::Unit::TestCase
+  def setup
+    @buildpack_path = File.expand_path 'test/fixtures/buildpacks/heroku-buildpack-ruby'
+  end
+
+  def test_can_deploy_git_app
+    lab_name = "websockets"
+    app = Hatchet::App.new("rails3_mri_193", labs: lab_name, buildpack: @buildpack_path)
+    app.setup!
+    assert(app.lab_is_installed?(lab_name), "Expected #{app.get_labs.inspect} to include {'name' => '#{lab_name}' } but it did not")
+  ensure
+    app.teardown! if app
+  end
+end
+
