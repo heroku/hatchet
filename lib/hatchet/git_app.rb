9fa184f34cb2be3afe299d5373d07a129bbe185a
schneems <richard.schneeman@gmail.com>
2014-01-27 15:34:35 -0600

    don't shell out unless we have to

--- lib/hatchet/git_app.rb
+++ lib/hatchet/git_app.rb
@@ -2,11 +2,11 @@ module Hatchet
   # used for deploying a test app to heroku via git
   class GitApp < App
     HATCHET_BUILDPACK_BASE   = (ENV['HATCHET_BUILDPACK_BASE'] || "https://github.com/heroku/heroku-buildpack-ruby.git")
-    HATCHET_BUILDPACK_BRANCH = (ENV['HATCHET_BUILDPACK_BRANCH'] || Hatchet.git_branch)
+    HATCHET_BUILDPACK_BRANCH = -> { ENV['HATCHET_BUILDPACK_BRANCH'] || Hatchet.git_branch }
     BUILDPACK_URL = "https://github.com/heroku/heroku-buildpack-ruby.git"
 
     def initialize(directory, options = {})
-      @buildpack = options[:buildpack] || options[:buildpack_url] || [HATCHET_BUILDPACK_BASE, HATCHET_BUILDPACK_BRANCH].join("#")
+      @buildpack = options[:buildpack] || options[:buildpack_url] || [HATCHET_BUILDPACK_BASE, HATCHET_BUILDPACK_BRANCH.call].join("#")
       super
     end
 
