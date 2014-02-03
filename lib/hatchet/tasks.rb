874ff93f66bf9ec765d17752e8ab57f38533b190
schneems <richard.schneeman@gmail.com>
2014-02-03 17:53:51 -0600

    set default email and username for git

--- lib/hatchet/tasks.rb
+++ lib/hatchet/tasks.rb
@@ -13,14 +13,16 @@ namespace :hatchet do
     puts "== Setting Up Travis =="
     [
      "bundle exec hatchet install",
+     %Q{ git config --global user.email ${"`git config --get user.name`"-'buildpack+tester@example.com'} },
+     %Q{ git config --global user.name  ${"`git config --get user.name`"-'Buildpack Tester'} },
      "echo '#{config_ssh}' >> ~/.ssh/config",
      "curl --fail --retry 3 --retry-delay 1 --connect-timeout 3 --max-time 30 https://toolbelt.heroku.com/install-ubuntu.sh | sh",
      "yes | heroku keys:add"
     ].each do |command|
       puts "== Running: #{command}"
       Bundler.with_clean_env do
-        `#{command}`
-        raise "Command failed: #{command.inspect}" unless $?.success?
+        result = `#{command}`
+        raise "Command failed: #{command.inspect}\nResult: #{result}" unless $?.success?
       end
     end
     puts "== Done =="
