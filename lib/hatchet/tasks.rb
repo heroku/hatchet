0eb26623df9183fa2854815498c82ff74b796ec2
schneems <richard.schneeman@gmail.com>
2017-08-11 21:25:44 -0500

    SSH not needed for CI

--- lib/hatchet/tasks.rb
+++ lib/hatchet/tasks.rb
@@ -1,20 +1,10 @@
-config_ssh = <<-RUBY
-
-Host heroku.com
-    StrictHostKeyChecking no
-    CheckHostIP no
-    UserKnownHostsFile=/dev/null
-Host github.com
-    StrictHostKeyChecking no
-RUBY
-
 namespace :hatchet do
-  task :setup_travis do
+  task :setup_ci do
     puts "== Setting Up Travis =="
-    netrc = "#{ENV['HOME']}/.netrc"
-    unless File.exists?(netrc)
-      File.open(netrc, 'w') do |file|
-        file.write <<EOF
+    netrc_file = "#{ENV['HOME']}/.netrc"
+    unless File.exists?(netrc_file)
+      File.open(netrc_file, 'w') do |file|
+        file.write <<-EOF
 machine git.heroku.com
   login #{ENV.fetch('HEROKU_API_USER')}
   password #{ENV.fetch('HEROKU_API_KEY')}
@@ -25,9 +15,6 @@ EOF
      "bundle exec hatchet install",
      "if [ `git config --get user.email` ]; then echo 'already set'; else `git config --global user.email '#{ENV.fetch('HEROKU_API_USER')}'`; fi",
      "if [ `git config --get user.name` ];  then echo 'already set'; else `git config --global user.name  'BuildpackTester'`      ; fi",
-     "echo '#{config_ssh}' >> ~/.ssh/config",
-     "ssh-keygen -f ~/.ssh/id_rsa -t rsa -N ''",
-     "yes | heroku keys:add -y"
     ].each do |command|
       puts "== Running: #{command}"
       Bundler.with_clean_env do
@@ -38,10 +25,7 @@ EOF
     puts "== Done =="
   end
 
-  task :teardown_travis do
-    ['heroku keys:remove'].each do |command|
-      puts "== Running: #{command}"
-      `#{command}`
-    end
+  task :setup_travis do
+    Rake::Task["hatchet:setup_ci"].invoke
   end
 end
