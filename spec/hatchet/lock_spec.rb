d232d842d09a4cb635c9bc0b0e5741a335dfdd3d
schneems <richard.schneeman+foo@gmail.com>
2020-07-01 09:56:11 -0500

    Add support for "main" branch

--- spec/hatchet/lock_spec.rb
+++ spec/hatchet/lock_spec.rb
@@ -2,6 +2,10 @@ require "spec_helper"
 require 'yaml'
 
 describe "LockTest" do
+  before(:all) do
+    puts(`bundle exec hatchet lock`)
+  end
+
   it "app with failure can be locked to prior commit" do
     Hatchet::GitApp.new("lock_fail").deploy do |app|
       expect(app.deployed?).to be_truthy
@@ -9,11 +13,18 @@ describe "LockTest" do
   end
 
   it "app with failure can be locked to master" do
-    puts(`bundle exec hatchet lock`)
     lock = YAML.load_file("hatchet.lock")
     name, branch = lock.select { |k, v| k.end_with?("lock_fail_master") }.first
 
     expect(name).to eq("repo_fixtures/repos/lock/lock_fail_master")
     expect(branch).to eq("master")
+  end
+
+  it "app with failure can be locked to main" do
+    lock = YAML.load_file("hatchet.lock")
+    name, branch = lock.select { |k, v| k.end_with?("lock_fail_main") }.first
+
+    expect(name).to eq("repo_fixtures/repos/lock/lock_fail_main")
+    expect(branch).to eq("main")
   end
 end
