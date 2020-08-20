17aac9f322d604284f0be674acb599b9a516ded7
schneems <richard.schneeman+foo@gmail.com>
2020-08-20 11:51:22 -0500

    Test `allow_failure` retry count behavior
    When `allow_failure` is set to true then it over-writes the value set by `retries`, this test asserts that to be true.

--- spec/hatchet/allow_failure_git_spec.rb
+++ spec/hatchet/allow_failure_git_spec.rb
@@ -13,17 +13,29 @@ describe "AllowFailureGitTest" do
     }
 
     it "is marked as a failure if the release fails" do
+      app = Hatchet::GitApp.new("default_ruby", before_deploy: release_fail_proc, retries: 2)
+      def app.retry_error_message(*args); @test_attempts_count ||= 0; @test_attempts_count += 1; "" end
+      def app.test_attempts_count; @test_attempts_count ; end
+
       expect {
-        Hatchet::GitApp.new("default_ruby", before_deploy: release_fail_proc, retries: 2).deploy {}
+        app.deploy {}
       }.to raise_error { |error|
         expect(error).to be_a(Hatchet::App::FailedReleaseError)
         expect(error.message).to_not match("Everything up-to-date")
       }
+
+      expect(app.test_attempts_count).to eq(2)
     end
 
     it "works when failure is allowed" do
-      Hatchet::GitApp.new("default_ruby", before_deploy: release_fail_proc, allow_failure: true).deploy do |app|
-        expect(app.output).to match("failing on release")
+      Hatchet::GitApp.new("default_ruby", before_deploy: release_fail_proc, allow_failure: true, retries: 3).tap do |app|
+        def app.retry_error_message(*args); @test_attempts_count ||= 0; @test_attempts_count += 1; "" end
+        def app.test_attempts_count; @test_attempts_count ; end
+
+        app.deploy do
+          expect(app.output).to match("failing on release")
+          expect(app.test_attempts_count).to eq(nil)
+        end
       end
     end
   end
