d2e56cce39fc1d0d2c94710455a921f7b612b2db
schneems <richard.schneeman+foo@gmail.com>
2020-09-09 15:10:39 -0500

    Fix branch detection on CircleCI
    We've been accidentally running all hatchet tests on CircleCI against `main`/`master` instead of the correct buildpack branch by accident.
    
    https://twitter.com/schneems/status/1303785823263883264

--- spec/hatchet/git_spec.rb
+++ spec/hatchet/git_spec.rb
@@ -6,4 +6,10 @@ describe "GitAppTest" do
       expect(app.output).to match("INTENTIONAL ERROR")
     end
   end
+
+  it "returns the correct branch name on circle CI" do
+    skip("only runs on circle") unless ENV["CIRCLE_BRANCH"]
+
+    expect(Hatchet.git_branch).to eq(ENV["CIRCLE_BRANCH"])
+  end
 end
