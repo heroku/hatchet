051e5c90aeaf7776692c0c28929f719ef6b2f78a
Ed Morley <501702+edmorley@users.noreply.github.com>
2021-01-25 18:46:34 +0000

    Use the standalone installer for 'ci:install_heroku' rather than APT (#171)
    Since the APT installer takes 3.5x longer than the non-APT version,
    and offers little benefit in a CI environment.
    
    Closes #169.

--- etc/setup_heroku.sh
+++ etc/setup_heroku.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
-curl --fail --retry 3 --retry-delay 1 --connect-timeout 3 --max-time 30 https://cli-assets.heroku.com/install-ubuntu.sh | sh
+curl --fail --retry 3 --retry-delay 1 --connect-timeout 3 --max-time 30 https://cli-assets.heroku.com/install.sh | sh
