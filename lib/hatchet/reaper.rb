3f13327a11def454088ffe231bfb2a2c67696be7
Richard Schneeman <rschneeman@salesforce.com>
2023-03-01 09:42:30 -0700

    Fix multiple duplicate deletion attempts (#198)
    * Fix multiple duplicate deletion attempts
    
    When multiple processes are attempting to cull the old apps list to free up enough space (to get under the limit), each will try to delete the same app.
    
    We already introduced a file based lock for multiple processes on the same machine. However the lock was not being used while clearing out old apps. That has been added.
    
    This file locking doesn't fix the problem, but it limits it instead of N machines running M processes, you only have to account for N machines. 
    
    The next fix is that when a duplicate deletion is detected, instead of having all machines continue on as fast as possible, the machines that "lost" the race will sleep to allow the winner to continue removing apps. When it wakes up it will refresh its API list and try again. This will happen in a loop until there are no more apps over TTL or it becomes the winner and other apps must sleep. This sleep logic uses the shared reaper backoff algorithm shared with the Platform gem detailed in my blog https://www.schneems.com/2020/07/08/a-fast-car-needs-good-brakes-how-we-added-client-rate-throttling-to-the-platform-api-gem/. When it begins to sleep, it will be a very small number, if there are lots of apps to be deleted and this triggers over and over, it will sleep longer and longer until it asymptotically reaches the max sleep value which is also the max TTL value. 
    
    The last change is that the @apps list (essentially a cache from the API) was not modified in this `destroy_older_apps` method so unless `refresh_app_list` was called every run to this method starts at the end and would trigger N `AlreadyDeleted` errors as it's in a data race with itself. This new code now removes an app from the list as they're being deleted to avoid this problem.
    
    * Introduce conflict strategy
    
    Turns out there was a big flaw in my logic. That existing rescue will effectively abort execution every time instead of continuing iteration. 
    
    I'm moving it inside the loop and introducing an `on_conflict` strategy that can either be `:sleep_and_continue` or `:stop_if_under_limit`. When a conflict is detected it always sleeps, but only refreshes the API if the limit hasn't yet been met.
    
    There's two main cases to consider
    
    - CLI: (calling `hatchet destroy --older-than=7`) In this case you want to to be extremely sure that ALL apps that are over TTL are deleted. For this case we use `:sleep_and_continue`
    - In the Hatchet App creation lifecycle: For every app we try to create, We first try to clean up old apps. The first time this triggers, it will hit the API and then cache the results. If there's an exception on app create then the app list is refreshed then we try to delete any old apps, but on conflict we can stop if we know there's enough spare capacity to create an app `:stop_if_under_limit`. After that, sleep if we're not yet down to the limit and retry the whole thing.
    
    * Rename on_conflict strategies
    
    The original reason conflict strategies were added was to gate whether or not we hit the API again. All times a conflict is hit a sleep occurs, so in the strategy `:sleep_and_continue`, the least important part is the sleep. I renamed it to put the behavior of refreshing the API front and center.
    
    Also updated bin/hatchet to make it explicit instead of relying on defaults.
    
    * v8.0.1

--- lib/hatchet/reaper.rb
+++ lib/hatchet/reaper.rb
@@ -28,6 +28,13 @@ module Hatchet
     DEFAULT_REGEX = /^#{Regexp.escape(Hatchet::APP_PREFIX)}[a-f0-9]+/
     TTL_MINUTES = ENV.fetch("HATCHET_ALIVE_TTL_MINUTES", "7").to_i
 
+    # Protect against parallel deletion on the same machine
+    # via concurrent processes
+    #
+    # Does not protect against distributed systems on different
+    # machines trying to delete the same applications
+    MUTEX_FILE = File.open(File.join(Dir.tmpdir(), "hatchet_reaper_mutex"), File::CREAT)
+
     attr_accessor :io, :hatchet_app_limit
 
     def initialize(api_rate_limit: , regex: DEFAULT_REGEX, io: STDOUT, hatchet_app_limit:  HATCHET_APP_LIMIT, initial_sleep: 10)
@@ -39,60 +46,127 @@ module Hatchet
       @reaper_throttle = ReaperThrottle.new(initial_sleep: initial_sleep)
     end
 
-    # Called when we need an app, but are over limit or
-    # if an exception has occured that was possibly triggered
-    # by apps being over limit
-    def clean_old_or_sleep
-      # Protect against parallel deletion of the same app on the same system
-      mutex_file = File.open("#{Dir.tmpdir()}/hatchet_reaper_mutex", File::CREAT)
-      mutex_file.flock(File::LOCK_EX)
-
-      destroy_older_apps(force_refresh: true)
-
-      if @apps.length > @limit
+    def sleep_if_over_limit(reason: )
+      if @apps.length >= @limit
         age = AppAge.new(created_at: @apps.last["created_at"], ttl_minutes: TTL_MINUTES)
         @reaper_throttle.call(max_sleep: age.sleep_for_ttl) do |sleep_for|
           io.puts <<-EOM.strip_heredoc
             WARNING: Hatchet app limit reached (#{@apps.length}/#{@limit})
-                     All known apps are younger than #{TTL_MINUTES} minutes
+            All known apps are younger than #{TTL_MINUTES} minutes.
+            Sleeping (#{sleep_for}s)
+
+            Reason: #{reason}
           EOM
 
           sleep(sleep_for)
         end
       end
-    ensure
-      mutex_file.close
     end
 
     # Destroys apps that are older than the given argument (expecting integer minutes)
-    def destroy_older_apps(minutes: TTL_MINUTES, force_refresh: @apps.empty?)
+    #
+    # This method might be running concurrently on multiple processes or multiple
+    # machines.
+    #
+    # When a duplicate destroy is detected we can move forward with a conflict strategy:
+    #
+    # - `:refresh_api_and_continue`: Sleep to see if another process will clean up everything for
+    #   us and then re-populate apps from the API and continue.
+    # - `:stop_if_under_limit`: Sleep to allow other processes to continue. Then if apps list
+    #   is under the limit, assume someone else is already cleaning up for us and that we're
+    #   good to move ahead to try to create an app. Otherwise if we're at or
+    #   over the limit sleep, refresh the app list, and continue attempting to delete apps.
+    def destroy_older_apps(minutes: TTL_MINUTES, force_refresh: @apps.empty?, on_conflict: :refresh_api_and_continue)
+      MUTEX_FILE.flock(File::LOCK_EX)
+
       refresh_app_list if force_refresh
 
-      @apps.each do |app|
+      while app = @apps.pop
         age = AppAge.new(created_at: app["created_at"], ttl_minutes: minutes)
-        if age.can_delete?
-          destroy_with_log(
-            name: app["name"],
-            id: app["id"],
-            reason: "app age (#{age.in_minutes}m) is older than #{minutes}m"
-          )
+        if !age.can_delete?
+          @apps.push(app)
+          break
+        else
+          begin
+            destroy_with_log(
+              id: app["id"],
+              name: app["name"],
+              reason: "app age (#{age.in_minutes}m) is older than #{minutes}m"
+            )
+          rescue AlreadyDeletedError => e
+            if handle_conflict(
+              strategy: on_conflict,
+              conflict_message: e.message,
+            ) == :stop
+              break
+            end
+          end
         end
-      rescue AlreadyDeletedError
-        # Ignore, keep going
       end
+    ensure
+      MUTEX_FILE.flock(File::LOCK_UN)
     end
 
     # No guardrails, will delete all apps that match the hatchet namespace
     def destroy_all(force_refresh: @apps.empty?)
+      MUTEX_FILE.flock(File::LOCK_EX)
+
       refresh_app_list if force_refresh
 
-      @apps.each do |app|
+      while app = @apps.pop
         begin
           destroy_with_log(name: app["name"], id: app["id"], reason: "destroy all")
-        rescue AlreadyDeletedError
-          # Ignore, keep going
+        rescue AlreadyDeletedError => e
+          handle_conflict(
+            conflict_message: e.message,
+            strategy: :refresh_api_and_continue
+          )
         end
       end
+    ensure
+      MUTEX_FILE.flock(File::LOCK_UN)
+    end
+
+    # Will sleep with backoff and emit a warning message
+    # returns :continue or :stop symbols
+    # :stop indicates execution should stop
+    private def handle_conflict(conflict_message:, strategy:)
+      message = String.new(<<-EOM.strip_heredoc)
+        WARNING: Possible race condition detected: #{conflict_message}
+        Hatchet app limit (#{@apps.length}/#{@limit}), using strategy #{strategy}
+      EOM
+
+      conflict_state = if :refresh_api_and_continue ==  strategy
+        message << "\nSleeping, refreshing app list, and continuing."
+        :continue
+      elsif :stop_if_under_limit == strategy && @apps.length >= @limit
+        message << "\nSleeping, refreshing app list, and continuing. Not under limit."
+        :continue
+      elsif :stop_if_under_limit == strategy
+        message << "\nHalting deletion of older apps. Under limit."
+        :stop
+      else
+        raise "No such strategy: #{strategy}, plese use :stop_if_under_limit or :refresh_api_and_continue"
+      end
+
+      @reaper_throttle.call(max_sleep: TTL_MINUTES) do |sleep_for|
+        io.puts <<-EOM.strip_heredoc
+          #{message}
+          Sleeping (#{sleep_for}s)
+        EOM
+
+        sleep(sleep_for)
+      end
+
+      case conflict_state
+      when :continue
+        refresh_app_list
+      when :stop
+      else
+        raise "Unknown state #{conflict_state}"
+      end
+
+      conflict_state
     end
 
     private def get_heroku_apps
@@ -117,15 +191,15 @@ module Hatchet
       body = e.response.body
       request_id = e.response.headers["Request-Id"]
       if body =~ /Couldn\'t find that app./
-        io.puts "Duplicate destroy attempted #{name.inspect}: #{id}, status: 404, request_id: #{request_id}"
-        raise AlreadyDeletedError.new
+        message = "Duplicate destroy attempted #{name.inspect}: #{id}, status: 404, request_id: #{request_id}"
+        raise AlreadyDeletedError.new(message)
       else
         raise e
       end
     rescue Excon::Error::Forbidden => e
       request_id = e.response.headers["Request-Id"]
-      io.puts "Duplicate destroy attempted #{name.inspect}: #{id}, status: 403, request_id: #{request_id}"
-      raise AlreadyDeletedError.new
+      message = "Duplicate destroy attempted #{name.inspect}: #{id}, status: 403, request_id: #{request_id}"
+      raise AlreadyDeletedError.new(message)
     end
   end
 end
